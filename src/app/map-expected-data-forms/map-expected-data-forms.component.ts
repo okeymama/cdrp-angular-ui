013db1cd0a485ff40eb72ced3931f5dcdb7f7636
udayv <udayv@USBLRUDAYV1.us.deloitte.com>
2019-02-07 17:22:35 +0530

    Changes to Add New Data Trajectory page

--- src/app/map-expected-data-forms/map-expected-data-forms.component.ts
+++ src/app/map-expected-data-forms/map-expected-data-forms.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import { FormData, AddData} from '../Study';
 import { AddExpectedDataComponent } from '../add-expected-data/add-expected-data.component';
-
+import { CdrpService } from '../cdrp.service';
 
 @Component({
   selector: 'app-map-expected-data-forms',
@@ -12,7 +12,7 @@ import { AddExpectedDataComponent } from '../add-expected-data/add-expected-data
 export class MapExpectedDataFormsComponent implements OnInit, OnDestroy {
 
   category: string[] = [];
-
+  mappingList = [];
   addData: AddData[] = [
   {
     'dataTrajectoryName': 'Data Trajectory Name 001',
@@ -28,7 +28,7 @@ export class MapExpectedDataFormsComponent implements OnInit, OnDestroy {
   }
   ];
   constructor(public dialog1: MatDialog,
-    public dialogRef: MatDialogRef<MapExpectedDataFormsComponent>,
+    public dialogRef: MatDialogRef<MapExpectedDataFormsComponent>, private cdrpService: CdrpService,
     @Inject(MAT_DIALOG_DATA) public data: FormData[]) {
       for (let c = 0; c < data.length; c++) {
         this.category[c] = '';
@@ -50,6 +50,7 @@ export class MapExpectedDataFormsComponent implements OnInit, OnDestroy {
   }
 
   openAddExpectedDataDialog(): void {
+    this.cdrpService.setSelectedMapping(this.mappingList);
     const dialogRef1 = this.dialog1.open(AddExpectedDataComponent, {
       width: '800px',
       height: '420px',
@@ -67,6 +68,11 @@ export class MapExpectedDataFormsComponent implements OnInit, OnDestroy {
   onSelect(selectedValue: any, ind1) {
     this.category[ind1] = selectedValue;
     console.log(this.category);
+    const i = this.mappingList.indexOf(selectedValue);
+    if (i <= -1) {
+      this.mappingList.push(selectedValue);
+    }
+    console.log(this.mappingList);
   }
 
 }
